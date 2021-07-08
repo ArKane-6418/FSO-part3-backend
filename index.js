672daaa30ab4abfc4ac866ac86589006c2c1b7b1
Joshua Ong <joshua.c.ong@hotmail.com>
2021-07-08 15:45:17 -0400

    Added linting and proper error checking

--- index.js
+++ index.js
@@ -99,6 +99,9 @@ const errorHandler = (error, request, response, next) => {
   else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   }
+  else if (error.name === 'MongoError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
