48e1778c374fb05062dff543b474db7f301a81c8
ongjoshu <joshua.ong@mail.utoronto.ca>
2021-07-10 15:47:35 -0400

    Added error handling for unknown endpoints

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ app.delete('/api/persons/:id', (request, response, next) => {
   // response.status(204).end()
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
